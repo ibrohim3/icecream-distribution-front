ba045cf5d46c6f2bdc8ca2efea1b2401686c93c1
ibrohim3 <ibrohim.2128382@gmail.com>
2025-12-21 00:51:20 +0500

    store create form added!

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:5000/api",
+    // baseURL: "http://localhost:5000/api",
+    baseURL: "https://icego.up.railway.app/api",
+    // baseURL: "https://icego.up.railway.app/api",
+    // baseURL: "https://icego.up.railway.app/api",
+    // baseURL: "https://icego.up.railway.app/api",
     // baseURL: "https://icego.up.railway.app/api",
 
     headers: {
