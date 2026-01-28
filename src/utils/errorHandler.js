6701c3f34aa915415bcb6815bf5ca25406f2829c
ibrohim3 <ibrohim.2128382@gmail.com>
2026-01-28 21:23:34 +0500

    done

--- src/utils/errorHandler.js
+++ src/utils/errorHandler.js
@@ -3,7 +3,7 @@ export const getErrorMessage = (error) => {
         return error.response.data.message || "Server xatosi"
     }
     if (error.request) {
-        return "Serverga ulanib bo‘lmadi"
+        return "Serverga ulanib bo'lmadi"
     }
-    return "Noma’lum xato"
+    return "Noma'lum xato"
 }
